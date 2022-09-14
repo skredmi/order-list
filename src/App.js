6489f27fc6da33726dbfb8d30b1e06b1f397cd70
Dmitriy Skrebkov <skredmi91@gmail.com>
2022-09-14 18:15:55 +0300

    change pre-commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1> <p>Hello world!</p>
+      <h1>Hello my world!</h1>
+      <p>Hello world!</p>
     </div>
   );
 }
